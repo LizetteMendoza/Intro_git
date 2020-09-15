a7308fc9d15b11eec2879f89f1a485489c791756
Lizette Mendoza <lizette.mendoza6892@alumnos.udg.mx>
2020-09-15 14:12:54 -0500

    Capturamos la edad e imprimimos la edad + 1

--- main.cpp
+++ main.cpp
@@ -4,11 +4,15 @@ using namespace std;
 
 int main(){
     char nombre[100];
+    unsigned int edad;
 
     cout<<"Hola mundo"<<endl;
     cout<<"Ingrese su nombre completo: ";
     cin.getline(nombre,sizeof(nombre),'\n');
     cout<<"Hola "<<nombre<<endl;
+    cout<<"ingrese su edad: ";
+    cin>>edad;
+    cout<<"Vas a cumpir "<<edad+1;
 
     return 0;
 }
