3b4f0f59ef3371a77ff33d9f8a1756ccaece00df
Lizette Mendoza <lizette.mendoza6892@alumnos.udg.mx>
2020-09-15 14:00:46 -0500

    Capturamos nombre completo usando getline

--- main.cpp
+++ main.cpp
@@ -3,8 +3,12 @@
 using namespace std;
 
 int main(){
+    char nombre[100];
 
     cout<<"Hola mundo"<<endl;
+    cout<<"Ingrese su nombre completo: ";
+    cin.getline(nombre,sizeof(nombre),'\n');
+    cout<<"Hola "<<nombre<<endl;
 
     return 0;
 }
